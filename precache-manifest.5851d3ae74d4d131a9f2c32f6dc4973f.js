da5d5d1d057fa5930012140f7284090c75ebceb4
Danielle Leblanc-Cyr <d.leblanc-cyr@creating-web.com>
2018-12-04 08:46:57 +0100

    Updates

---
+++ precache-manifest.5851d3ae74d4d131a9f2c32f6dc4973f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2ecf2e68de309a6bd2b",
+    "url": "/react-calculator/static/js/runtime~main.a2ecf2e6.js"
+  },
+  {
+    "revision": "0f0eee0873b088c11e90",
+    "url": "/react-calculator/static/js/main.0f0eee08.chunk.js"
+  },
+  {
+    "revision": "0cf0fb26a2c5988a2594",
+    "url": "/react-calculator/static/js/1.0cf0fb26.chunk.js"
+  },
+  {
+    "revision": "0f0eee0873b088c11e90",
+    "url": "/react-calculator/static/css/main.0bc6a7f1.chunk.css"
+  },
+  {
+    "revision": "725ff93e488ad3d57cf552b5cdecdce9",
+    "url": "/react-calculator/index.html"
+  }
+];
